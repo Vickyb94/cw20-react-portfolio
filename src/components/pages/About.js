64e77c781b5ff702434fdf477e1cc52dcd55a529
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2023-04-03 15:45:17 -0300

    added the about page

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+        <h1>About Me</h1>
+        <p>I am a full stack developer. 
+           I have an MBA degree from Westcliff University.
+           I create and maintain responsive secure websites
+        </p>
+    </div>
+  );
+}
+
+export default About;
