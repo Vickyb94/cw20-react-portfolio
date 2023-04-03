3c309f25df2a82094881c0f526945772f36bebd0
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2023-04-03 17:19:35 -0300

    installed react-router-dom and react icons

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import Avatar from "../assets/avatar.png";
+
 const About = () => {
   return (
     <div>
         <h1>About Me</h1>
+        <img src={Avatar} className="img" alt="true"/>
         <p>I am a full stack developer. 
            I have an MBA degree from Westcliff University.
            I create and maintain responsive secure websites
---
+++ src/components/pages/Contact.js
@@ -0,0 +1,3 @@
+import React, { useState } from "react";
+
+import { useForm, ValidationError } from "@form"
