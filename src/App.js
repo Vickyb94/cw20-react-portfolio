3b07a0c015d9a74ac75f97edac276617f0f2d7e1
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2023-04-03 20:05:27 -0300

    created the footer with social icons using react-icons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import React from 'react';
 import NavTab from './components/NavTabs';
+import Footer from './Footer';
 
 
 function App() {
   return (
     <div>
       <NavTab />
+      <Footer />
       
     
     </div>
---
+++ src/Footer.js
@@ -0,0 +1,36 @@
+
+import React from 'react';
+//import icons
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+        <div className="social">
+            <a  href="https://github.com/Vickyb94">
+              <FaGithub  
+                 size={30}
+                 style={{ color: "#fff",
+                 marginRight: "1rem"}}/>
+            </a>
+            <a href="https://www.linkedin.com/in/victoria-developer/">
+                <FaLinkedin
+                    size={30}
+                    style={{ color: "#fff",
+                    marginRight: "1rem"}}/>
+               </a>
+            <a href="https://twitter.com/eji_row">
+               <FaTwitter
+                  size={30}
+                  style={{ color: "#fff",
+                  marginRight: "1rem"}}/>
+            </a>
+        </div>
+        <div className="copyright">
+            <p>Vickyb94@2023</p>
+        </div>
+    </div>
+  )
+}
+
+export default Footer;
