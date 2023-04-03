17b855a74e25b78a60cf736cba90b78a482fb558
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2023-04-03 20:27:32 -0300

    added an avatar to the about page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import React from 'react';
 import NavTab from './components/NavTabs';
 import Footer from './Footer';
+import About from './components/pages/About';
 
 
 function App() {
   return (
     <div>
       <NavTab />
+      <About />
       <Footer />
       
     
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,15 +1,23 @@
 import React from 'react';
 
-//import Avatar from "../assets/avatar.png";
+import Avatar from "../assets/avatar.png";
 
 const About = () => {
   return (
-    <div>
-        <h1>About Me</h1>
-        <p>I am a full stack developer. 
-           I have an MBA degree from Westcliff University.
-           I create and maintain responsive secure websites
-        </p>
+    <div className="about">
+        <div className="left">
+            <h1>About Me</h1>
+                <p>I prefer to be called Vicky and I am a full stack developer. 
+                   I have an MBA degree from Westcliff University.
+                   I create and maintain responsive secure websites
+                </p>
+        </div>
+        <div className="right">
+            <div className="img-container">
+              <img src={Avatar} className="img" alt="true"/>
+            </div>
+        </div>
+
     </div>
   );
 }
