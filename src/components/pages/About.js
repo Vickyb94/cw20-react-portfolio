6af046367cc2cb5f81983ffed447c7b26292bf64
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2023-04-03 19:01:12 -0300

    edited app.js to render header and footer

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-import Avatar from "../assets/avatar.png";
+//import Avatar from "../assets/avatar.png";
 
 const About = () => {
   return (
     <div>
         <h1>About Me</h1>
-        <img src={Avatar} className="img" alt="true"/>
         <p>I am a full stack developer. 
            I have an MBA degree from Westcliff University.
            I create and maintain responsive secure websites
